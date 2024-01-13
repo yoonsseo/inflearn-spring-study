7bf8afeaf6ec635daca8a672c58f5ab240c097d5
yoonsseo <yellisa2000@gmail.com>
2024-01-14 03:24:47 +0900

    feat: 실무 활용 - 순수 JPA와 Querydsl

---
+++ src/main/java/spring/querydsl/controller/MemberController.java
@@ -0,0 +1,22 @@
+package spring.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.querydsl.dto.MemberSearchCondition;
+import spring.querydsl.dto.MemberTeamDto;
+import spring.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+
+}
